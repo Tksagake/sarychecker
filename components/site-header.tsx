dc549bb7e92c290423373240775581a237d1098d
Sikolia Malcolm <malcolmsikolia@gmail.com>
2025-05-09 15:18:29 +0300

    finally 2.1

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { UserNav } from '@/components/user-nav';
 import { MainNav } from '@/components/main-nav';
 import { ModeToggle } from '@/components/mode-toggle';
+import Image from 'next/image';
 import { BuildingIcon } from 'lucide-react';
 
 export function SiteHeader() {
@@ -9,7 +10,15 @@ export function SiteHeader() {
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
         <Link href="/" className="flex items-center space-x-2">
-          <BuildingIcon className="h-6 w-6 text-primary" />
+          
+          <Image
+            src="https://sary.co/static/media/logo.0c4f2a3b.svg"
+            alt="Sary Logo"
+            width={40}
+            height={40}
+            className="hidden h-8 w-8 rounded-full sm:inline-block" >
+
+            </Image>
           <span className="hidden font-bold sm:inline-block">
             Sary Network International
           </span>
