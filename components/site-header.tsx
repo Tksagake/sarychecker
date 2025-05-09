c2b79ef2c8271f37048cd8862250397d8f9316d8
Sikolia Malcolm <malcolmsikolia@gmail.com>
2025-05-09 15:35:16 +0300

    mvp 1.0

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,34 +1,21 @@
 import Link from 'next/link';
-import { UserNav } from '@/components/user-nav';
 import { MainNav } from '@/components/main-nav';
 import { ModeToggle } from '@/components/mode-toggle';
 import Image from 'next/image';
-import { BuildingIcon } from 'lucide-react';
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
-        <Link href="/" className="flex items-center space-x-2">
-          
+      <div className="container flex h-16 items-center justify-center">
+        <Link href="/" className="flex items-center">
           <Image
             src="/logo.png"
             alt="Sary Logo"
             width={40}
             height={40}
-            className="hidden h-8 w-8 rounded-full sm:inline-block" >
-
-            </Image>
-          <span className="hidden font-bold sm:inline-block">
-            Sary Network International
-          </span>
-          <span className="inline-block font-bold sm:hidden">Sary</span>
+            className="h-8 w-8 rounded-full"
+          />
         </Link>
-        <MainNav className="mx-6" />
-        <div className="ml-auto flex items-center space-x-4">
-          <ModeToggle />
-          <UserNav />
-        </div>
       </div>
     </header>
   );
